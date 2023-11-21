1f4a3b968d22e299d3657a5ade07d57459d8e6b4
Łukasz Sadłocha <lukaszsadlocha@gmail.com>
2023-11-21 23:24:00 +0100

    Adds project to receive and send messages to Queue in Azure Service Bus

---
+++ RetirementHome.ServiceBus.Sender/Program.cs
@@ -0,0 +1,66 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+namespace RetirementHome.ServiceBus.Sender
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build();
+
+
+            var azServiceBusConnString = configuration.GetConnectionString("AzureServiceBus");
+            var queueName = configuration.GetSection("queueName").Value;
+
+            var client = new ServiceBusClient(azServiceBusConnString);
+
+            // Sending to Azure ServiceBus
+            var sender = client.CreateSender(queueName);
+
+            var stingToSent = $"This message was prepared at: {DateTime.UtcNow}";
+
+            var message = new ServiceBusMessage(stingToSent);
+
+            Console.WriteLine("Sending...");
+            await sender.SendMessageAsync(message, CancellationToken.None);
+
+            await sender.CloseAsync();
+
+            Console.WriteLine("Sent\n");
+
+
+            // Receiving
+            var receiver = client.CreateReceiver(queueName);
+
+            var cts = new CancellationTokenSource(10000);
+            var cancellationToken = cts.Token;
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                var receivedMessage = await receiver.ReceiveMessageAsync();
+
+                if(receivedMessage != null)
+                {
+                    Console.WriteLine($"Received message: {receivedMessage.Body}");
+
+                    //Complete the messae
+                    await receiver.CompleteMessageAsync(receivedMessage);
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("All messages received.");
+                    break;
+                }
+            }
+
+            // Close the receiver
+            await receiver.CloseAsync();
+        }
+    }
+}
+
